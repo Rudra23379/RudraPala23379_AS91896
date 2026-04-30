375227aa69383d3e9765aa63524ec70975756c49
23379 <23379@students.mrgs.school.nz>
2026-04-30 13:29:54 +1200

    tk

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ root = tk.Tk()
 
 root.title('My flages quiz')
 
-label = tk.Label(root, text='My flages quiz')
-label.pack()
+label = tk.Label(root, text='Welcome To My Flag Quiz', font=("Arial", 40))
+label.pack(pady=20)
+
+try:
+photo = tk.PhotoImage(file='./images/Title_forest.jpg.png')
+
+image_label = tk.Label(root, image=photo)
+image_label.pack()
 
 root.mainloop()
