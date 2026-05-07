0263fa15c7d48623909ba4c461f57513a1b230a4
23379 <23379@students.mrgs.school.nz>
2026-05-07 13:30:06 +1200

    title page background passs

--- Homepage.py
+++ Homepage.py
@@ -1,31 +1,39 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-root = tk.Tk()
-root.title('My flags quiz')
-root.geometry("800x600")
 
-parent = tk.Tk()
-parent.title("Application Title")
 
-label = tk.Label(root, text='Welcome To My Flag Quiz', font=("Arial", 40))
-label.pack(pady=20)
 
-# Load and convert image
-Homescreen_image = Image.open("images/Title_forest.jpg")
-Homescreen_image=Homescreen_image.resize ((1950,1200))
-tk_image = ImageTk.PhotoImage(Homescreen_image)
 
-background_label = tk.Label(root, image=tk_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+class Homescreen:
+    root = tk.Tk()
+    root.title("My flags quiz")
+    root.geometry("800x600")
 
-# Display image
-image_label = tk.Label(root, image=tk_image)
-image_label.image = tk_image
-image_label.pack()
+    # Load and convert image
+    homescreen_image = Image.open("images/evenevenevenevenevenbetterhomepagesharp.png")
+    homescreen_image = homescreen_image.resize((1950, 1200))
+    tk_image = ImageTk.PhotoImage(homescreen_image)
 
-Label= tk.Label(root, text='My Flag Quiz', font=("Arial", 40))
+    background_label = tk.Label(root, image=tk_image)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-parent.mainloop()
+    # Display image
+    image_label = tk.Label(root, image=tk_image)
+    image_label.image = tk_image
+    image_label.pack()
 
-root.mainloop()
+    canvas = tk.Canvas(root, width=800, height=600, highlightthickness=0)
+    canvas.pack()
+    canvas.create_image(0, 0, image=tk_image, anchor="nw")
+
+    label = tk.Label(root, text="Welcome To My Flag Quiz", font=("Arial", 40))
+    label.pack(pady=20)
+
+    Label = tk.Label(root, text="My Flag Quiz", font=("Arial", 40))
+
+
+
+
+
+    root.mainloop()
