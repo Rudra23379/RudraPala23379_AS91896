0ea24d0ebc77ee32fa54376610f79c37f162769d
23379 <23379@students.mrgs.school.nz>
2026-05-19 09:51:34 +1200

    Restart 2

--- Homepage.py
+++ Homepage.py
@@ -1,12 +1,40 @@
-import tkinter as tk
-import PIL.ImageTk
+import customtkinter as ctk
+from PIL import Image, ImageTk
 
-root = tk.Tk()
-root.title("Homepage")
-root.attributes("-fullscreen", True)
-quitButton = tk.Button(root, text="Quit", command=root.destroy, padx=35, pady=25)
-quitButton.place(x=85, y=100)
+root= ctk.CTk()
+root.after(0, lambda: root.state('zoomed'))
 
-Helpbutton = tk.Button(root, text="Help", padx=35, pady=25)
-Helpbutton.place(x=400, y=150)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+bg_image = Image.open("images/firewatch.jpg")
+bg_image = ctk.CTkImage(light_image=bg_image, dark_image=bg_image, size=(screen_width, screen_height))
+bg_label = ctk.CTkLabel(root, image=bg_image, text="")
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+root.title("My Flag Quiz")
+title_text = ctk.CTkLabel(root, text="Welcome to my flag quiz",
+font=("Arial", 56, "bold"), text_color="#1a5c3a",fg_color="transparent")
+title_text.place(x=600, y=90)
+
+
+quit_button = ctk.CTkButton(root, text="Quit", command=root.quit, text_color="#ffffff", corner_radius=27, width=160, height=40,)
+quit_button.place(x=15, y=35)
+
+
+quit_icon = ctk.CTkButton(root,text="⏻", width=64, height=64,corner_radius=32, command=root.quit,font=("CanvaSans", 32, "bold"))
+quit_icon.place(x=300, y=400)
+
+help_icon = ctk.CTkButton(root, text="?", width=64,height=64,corner_radius=32,font=("CanvaSans", 32, "bold"),)
+help_icon.place(x=200, y=200)
+
+
+
+help_button = ctk.CTkButton(root, text="Help" )
+help_button.place(x=1400, y=35)
+
+username = ctk.CTkEntry(root, placeholder_text="please enter your name here", width=320,height=50)
+username.place(x=400, y=450)
+
+
 root.mainloop()
