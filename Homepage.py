f7f314721e2c85ef114018c314f16a81b2da8c79
23379 <23379@students.mrgs.school.nz>
2026-05-29 11:47:01 +1200

    Diff page 2

--- Homepage.py
+++ Homepage.py
@@ -23,10 +23,10 @@ class Flagquiz:
 
         # Title
         root.title("My Flag Quiz")
-        self.title_text = ctk.CTkLabel(self.bg_label, text="Welcome to my flag quiz",
+        self.title_text = ctk.CTkLabel(self.bg_label, text="Welcome To My Flag Quiz",
                                   font=("CanvaSans", 56, "bold"), text_color="#1a5c3a", fg_color="transparent",
                                   bg_color="#c8e690")
-        self.title_text.place(relx=0.49, rely=0.15, anchor="center")
+        self.title_text.place(relx=0.46, rely=0.15, anchor="center")
 
         #Quit button
         quit_button = ctk.CTkButton(self.bg_label, text="Quit", command=root.quit,
@@ -42,19 +42,20 @@ class Flagquiz:
                                   bg_color="#c8e690", hover_color="#ffffff", fg_color="#2d6349")
         quit_icon.place(relx=0.06, rely=0.08, anchor="center")
         #help button
-        help_icon = ctk.CTkButton(self.bg_label, text="?", width=64, height=64, corner_radius=32,
-                                  font=("CanvaSans", 32, "bold"), bg_color="#c8e690", hover_color="#ffffff",
+        help_icon = ctk.CTkButton(self.bg_label, text=" ? ", width=64, height=64, corner_radius=32,
+                                  font=("CanvaSans", 36, "bold"), bg_color="#c8e690", hover_color="#ffffff",
                                   fg_color="#2d6349")
         help_icon.place(relx=0.94, rely=0.08, anchor="center")
         #Help button icon
-        help_button = ctk.CTkButton(self.bg_label, text="Help", bg_color="transparent", hover_color="#ffffff",
-                                    fg_color="#2d6349")
+        help_button = ctk.CTkButton(self.bg_label, text="Help", bg_color="#c8e690", hover_color="#ffffff",
+                                    fg_color="#2d6349", font=("CanvaSans", 22, "bold"),corner_radius= 32, width=160,height=40,
+                                    )
         help_button.place(relx=0.94, rely=0.16, anchor="center")
 
         # Username Input
         self.username = ctk.CTkEntry(self.bg_label, placeholder_text="please enter your name here",
                                 width=320, height=50, justify="center", corner_radius=32, text_color="#ffffff",
-                                placeholder_text_color="#ffffff", fg_color="#2d6349", bg_color="#2d6349")
+                                placeholder_text_color="#ffffff", fg_color="#2d6349", bg_color="#0b3835")
         self.username.place(relx=0.5, rely=0.7, anchor="center")
         #start button
         self.start_button = ctk.CTkButton(self.bg_label, text="start", corner_radius=32, width=220, height=70, bg_color="#2d6349",
@@ -62,7 +63,7 @@ class Flagquiz:
         self.start_button.place(relx=0.49, rely=0.5, anchor="center")
 
     def diff(self):
-
+            # Username return
             player_name = self.username.get().strip()
             if player_name in("","please enter your name here",):
                 print("please enter a vaild name")
